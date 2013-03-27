6fabd12c61431d24e36d40c925faa9be51036989
Seivan Heidari <seivan@kth.se>
2013-03-27 14:20:24 +0100

    Initial Commit

---
+++ SHOmniAuthTwitterExample/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  SHOmniAuthTwitterExample
+//
+//  Created by Seivan Heidari on 3/27/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
